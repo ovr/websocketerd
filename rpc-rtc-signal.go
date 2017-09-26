9746070110221b044c393c52f0768200b7ed1fbe
Dmitry Patsura <talk@dmtry.me>
2017-09-26 17:31:17 +0900

    [Feature] RPCRTCSignalHandler - check signalType

--- rpc-rtc-signal.go
+++ rpc-rtc-signal.go
@@ -37,14 +37,20 @@ func (this RPCRTCSignalHandler) Parameters() []RPCParameter {
 
 func (this RPCRTCSignalHandler) Handle(request *RPCRequest, client *Client) (*JSONMap, error) {
 	signalType := request.Parameters[0].(string)
+
+	if signalType != "RTC_OFFER" && signalType != "RTC_ANSWER" && signalType != "RTC_ICE_CANDIDATE" {
+		return nil, errors.New("signalType must be [RTC_OFFER, RTC_ANSWER, RTC_ICE_CANDIDATE]")
+	}
+
 	roomId := request.Parameters[1].(string)
-	from := request.Parameters[2].(string)
-	to := request.Parameters[3].(string)
-	payload := request.Parameters[4].(JSONMap)
 
 	if strings.Contains(roomId, "*") {
 		return nil, errors.New("Pattern * is not allowed")
 	}
+
+	from := request.Parameters[2].(string)
+	to := request.Parameters[3].(string)
+	payload := request.Parameters[4].(JSONMap)
 
 	channel := fmt.Sprintf("room:%s", roomId)
 
